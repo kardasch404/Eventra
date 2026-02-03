2fc0c1e8d223311d6b386827e61e125974d60bdb
kardasch404 <zz2406143@gmail.com>
2026-02-03 11:31:27 +0100

    feat: configure redis module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphqlConfigModule } from '@presentation/graphql/graphql.module';
 import { AuthModule } from '@presentation/graphql/auth.module';
+import { RedisModule } from '@infrastructure/redis.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), GraphqlConfigModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), RedisModule, GraphqlConfigModule, AuthModule],
   controllers: [],
   providers: [],
 })
